49385bd06c60b26f9be54b0eb19820f8b1b08129
shahongzhou <378221883@qq.com>
2017-04-14 11:19:14 +1200

    JuniTest

--- src/JUnitTest/UserTest.java
+++ src/JUnitTest/UserTest.java
@@ -1,14 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package JUnitTest;
 
-/**
- *
- * @author MengMeng
- */
-public class UserTest {
+
+
+
+import LogInUI.User;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserTest  {
+    private User user;
+     @Before
+     public void setUp() {
+        user = new User("name");
+        
+     }
     
+    @Test
+    public void testName(){      
+         String name = "name";
+         String name1 = user.getName();
+         assertEquals(name, name1);
+     }
+    
+    @Test
+    public void testScore(){
+        int score = 100;
+        user.setScore(score);
+        int score1 = user.getScore();
+        assertEquals(score, score1);
+        
+    }
+    
+    
+
 }
